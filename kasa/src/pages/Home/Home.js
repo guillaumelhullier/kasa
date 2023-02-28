8a19e9c730a9de82990feb6883cf04c394c120a4
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2023-02-28 17:19:04 +0100

    pageHome

--- kasa/src/pages/Home/Home.js
+++ kasa/src/pages/Home/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Banner from "../../components/Banner/Banner";
+import Cards from "../../components/Cards/Cards";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="mainContainer">
+      <div className="container">
+        <Header />
+        <Banner />
+        <Cards />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
