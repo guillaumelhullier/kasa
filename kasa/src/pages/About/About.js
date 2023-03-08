8287823e621f6b213b72597f841bfda1349aa0e8
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2023-03-08 17:06:55 +0100

    About

--- kasa/src/pages/About/About.js
+++ kasa/src/pages/About/About.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/Banner";
 import Footer from "../../components/Footer/Footer";
+import Collapse from "../../components/Collapse/Collapse";
+import CssAbout from "./About.module.css";
 
 const About = () => {
   return (
@@ -9,6 +11,35 @@ const About = () => {
       <div className="container">
         <Header />
         <Banner />
+        <section className={CssAbout.collapseAboutContainer}>
+          <Collapse
+            title="Fiabilité"
+            text="Les annonces postées sur Kasa garantissent une fiabilité totale.
+          Les photos sont conformes aux logements, et toutes les
+          informations sont régulièrement vérifiées par nos équipes."
+          />
+          <Collapse
+            title="Respect"
+            text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme."
+          />
+          <Collapse
+            title="Service"
+            text="Nos équipes se tiennent à votre disposition pour vous fournir une
+          expérience parfaite. N'hésitez pas à nous contacter si vous avez la
+          moindre question."
+          />
+          <Collapse
+            title="Sécurité"
+            text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+          pour les voyageurs, chaque logement correspond aux critères de
+          sécurité établis par nos services. En laissant une note aussi bien à
+          l'hôte qu'au locataire, cela permet à nos équipes de vérifier que
+          les standards sont bien respectés. Nous organisons également des
+          ateliers sur la sécurité domestique pour nos hôtes."
+          />
+        </section>
       </div>
       <Footer />
     </div>
