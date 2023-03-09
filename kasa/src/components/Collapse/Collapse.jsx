ad8037867c9a4f60fafe2c8f65f8def7fe4d1888
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2023-03-09 17:29:58 +0100

    Collapse

--- kasa/src/components/Collapse/Collapse.jsx
+++ kasa/src/components/Collapse/Collapse.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import CssCollapse from "./Collapse.module.css";
+import Arrow from "../../assets/arrowIcon.svg";
 
-const Collapse = () => {
-  return <div></div>;
-};
+function Collapse({ title, text }) {
+  const [arrowUp, setArrowUp] = useState(false);
+
+  return arrowUp ? ( //Développement d'une ternaire
+    <div className={CssCollapse.collapseContainer}>
+      <div className={CssCollapse.title} onClick={() => setArrowUp(false)}>
+        {title}
+        <img
+          src={Arrow}
+          className={CssCollapse.iconUp}
+          alt="arrow up"
+          role="button"
+        />
+      </div>
+      <div className={CssCollapse.text}>{text}</div>
+    </div>
+  ) : (
+    //Chargement du hooks useState
+    <div className={CssCollapse.collapseContainer}>
+      <div className={CssCollapse.title} onClick={() => setArrowUp(true)}>
+        {title}
+        <img
+          src={Arrow}
+          className={CssCollapse.iconDown}
+          alt="arrow down"
+          role="button"
+        />
+      </div>
+    </div>
+  );
+}
 
 export default Collapse;
